be700c31b250f89272df749e8c57b6516a60a727
Ferousco-dev <ferouslos6@gmail.com>
2026-06-24 23:59:05 +0100

    Add sticky navbar that slides in on scroll past hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
-import { Hero } from "./components/Hero";
-import { About } from "./components/About";
+import { StickyNav }   from "./components/StickyNav";
+import { Hero }         from "./components/Hero";
+import { About }        from "./components/About";
 import { CoreFeatures } from "./components/CoreFeatures";
 import { PersonaCards } from "./components/PersonaCards";
-import { Roadmap } from "./components/Roadmap";
-import { CTA } from "./components/CTA";
-import { Footer } from "./components/Footer";
+import { Roadmap }      from "./components/Roadmap";
+import { CTA }          from "./components/CTA";
+import { Footer }       from "./components/Footer";
 
 export default function Home() {
   return (
     <main>
+      <StickyNav />
       <Hero />
       <About />
       <CoreFeatures />
